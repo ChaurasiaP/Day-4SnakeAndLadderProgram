2dc8ac93dc19e869db888a0aed45ea798b100048
ChaurasiaP <pranshu1431@gmail.com>
2023-01-30 16:40:57 +0530

    WELCOME TO SNAKE AND LADDER PROGRAM

---
+++ SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package Day4SnakeAndLadder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO DAY:4 SNAKE AND LADDER PROGRAM");
+    }
+}
