1b84049e314b684e578a300aa53795cc3d81e214
ChaurasiaP <pranshu1431@gmail.com>
2023-01-30 17:10:33 +0530

    added UC2

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,15 @@
 package Day4SnakeAndLadder;
 
 public class SnakeAndLadder {
+    static int dice;     // VARIABLES TO STORE THE RANDOM VALUES FOR PLAY CONDITIONS AND DICE ROLLS
+
+    public static void diceRoll() {         // FUNCTION TO ROLL THE DICE
+
+        dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+    }
     public static void main(String[] args) {
         int playerPosition = 0;         // START POSITION
         System.out.println("Start position: " + playerPosition);
+        diceRoll();
     }
 }
