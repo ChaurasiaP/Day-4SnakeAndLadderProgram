b81ce59e78b06156f9b54299b87a13821ece37e3
ChaurasiaP <pranshu1431@gmail.com>
2023-01-30 17:22:01 +0530

    added UC5 and UC6 problems

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,46 +9,64 @@ public class SnakeAndLadder {
 
     static int check, dice;     // VARIABLES TO STORE THE RANDOM VALUES FOR PLAY CONDITIONS AND DICE ROLLS
 
-
     public static int checkMove() {         // FUNCTION TO CHECK FOR PLAYING CONDITION
         check = (int) Math.floor((Math.random()) * 10) % 3;
         return check;
     }
 
     public static void diceRoll() {         // FUNCTION TO ROLL THE DICE
-
         dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
     }
 
+
     public static void main(String[] args) {
+
+        int count = 0;          // COUNTER TO GET TOTAL DICE ROLLS
         int playerPosition = 0;         // START POSITION
         System.out.println("Start position: " + playerPosition);
 
         while (playerPosition < win) {
+
             switch (checkMove()) {          // CHECK FOR NO PLAY/ LADDER/ SNAKE CONDITIONS
                 case NO_PLAY:
                     System.out.println("uh-Oh No play, Try Again!!");
                     playerPosition += 0;
                     System.out.println("your position " + playerPosition);
                     System.out.println(" ");
+                    count++;
                     break;
 
                 case LADDER:
                     diceRoll();         // DICE ROLL
-                    System.out.println("Dice is rolled and the number is: " + dice);
-                    System.out.println("Its a ladder, Move ahead with " + dice + " position");
-                    playerPosition += dice;         // INCREMENT THE POSITION
-                    System.out.println("your position " + playerPosition);
-                    System.out.println(" ");
+                    if (0 < (playerPosition + dice) && (playerPosition + dice) <= 100) {        // PLAYER POSITION SHOULD BE POSITIVE AND <= 100
+                        System.out.println("Dice is rolled and the number is: " + dice);
+                        System.out.println("Its a ladder, Move ahead with " + dice + " position");
+                        playerPosition += dice;         // INCREMENT THE POSITION
+                        System.out.println("your position " + playerPosition);
+                        System.out.println(" ");
+                        count++;            // DICE ROLL COUNTER
+                    }
                     break;
                 case SNAKE:
                     diceRoll();
-                    System.out.println("Dice is rolled and the number is: " + dice);
-                    System.out.println("Oops! you got bitten by a snake Move back with " + dice + " position");
-                    playerPosition -= dice;
-                    System.out.println("Your position " + playerPosition);
+                    if (playerPosition - dice > 0) {            // WILL WORK ONLY IF POSITION IS POSITIVE
+                        System.out.println("Dice is rolled and the number is: " + dice);
+                        System.out.println("Oops! you got bitten by a snake Move back with " + dice + " position");
+                        playerPosition -= dice;
+                        System.out.println("Your position " + playerPosition);
+                    } else {            // IF POSITION IS NEGATIVE IT WILL RESTART FROM ZERO
+                        playerPosition = 0;
+                        System.out.println("Dice is rolled and the number is: " + dice);
+                        System.out.println("TOO BAD!!! you got bitten BAD!! Start from the BEGINNING!!!");
+                    }
+                    System.out.println(" ");
+                    count++;
+                    break;
             }
-
+            if (playerPosition == 100) {        // WINNING CONDITION IS ACHIEVED
+                System.out.println("Congratulations!!!! YOU HAVE WON AFTER " + count + " MOVES");
+                break;
+            }
         }
     }
 }
